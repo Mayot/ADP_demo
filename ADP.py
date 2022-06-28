62631b0746f984cb006cb6bf875e1b73cdf6b4b9
Fr.svc Devisic <paul.emile@kuehne-nagel.com>
2022-06-28 16:26:39 +0200

    coucou

--- ADP.py
+++ ADP.py
@@ -5,37 +5,43 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains #pour créer des enchainements d'action, notamment mouseover
-from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.common.action_chains import ActionChains #pour créer des enchainements d'action, notamment mouseover
+#from selenium.webdriver.support.ui import WebDriverWait #pour faire des temps d'attentes de chargement de page
 from selenium.webdriver.support.ui import Select #pour gérer les différents tabs
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.chrome.options import Options pour les option
 from unidecode import unidecode #for unicode conversion
-from datetime import datetime, timedelta #For date
+#from datetime import datetime, timedelta #For date
 import time #for sleep
-import re #for Regex
+#import re #for Regex
 import logging #for logging
 import os #for listing file
 #from mail import send_mail_with_attm, send_mail #lib perso pour envoi de mail
 
 
-
-
 # Variables initiales
-app_name = "ADP"
-workspace = "C:/Apps/"+app_name+"/"
+app_name = "ADP_demo"
+workspace = "D:/workspace/python/"+app_name+"/"
 download_folder = "C:\\Apps\\"+app_name
+with open(workspace+'credentials.txt') as f:
+    myCred = []
+    for ligne in f:
+        myCred.append(ligne.strip())
 
 
 # Variables à modifier
-url = os.getenv('url', default=None)
+#url = os.getenv('url', default=None)
+url = 'https://hr-services.fr.adp.com/portal-main/portal/#/home'
+url = "https://hr-services.fr.adp.com/login/?TARGET=-SM-https%3a%2f%2fhr--services%2efr%2eadp%2ecom%2f_index%2fportal--main%2fportal%2f#/home"
 username = os.getenv('ADP_user', default=None)
+username = myCred[0]
 pwd = os.getenv('ADP_pwd', default=None)
+pwd = myCred[1]
 
 
 # Début du programme 
 
-
+#Log
 #filemode : a = append, w = overwrite every run
 logging.basicConfig(filename=workspace+app_name+'_log.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 #Let us Create an object 
@@ -45,6 +51,8 @@ logger.setLevel(logging.INFO) #DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 logger.info("START START START START START START START START START")
 #logger.info(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+
+
 
 #Read date.txt file (contains all dates to add in ADP) and get all the line
 with open(workspace+'date.txt') as f:
@@ -62,6 +70,7 @@ with open(workspace+'date.txt') as f:
 logger.info('Fichier contenant '+str(len(myFile))+' lignes.')
 
 
+
 #si pas de donnée on fait rien
 if len(myFile) > 0:
     ### Démarage du programme
@@ -69,7 +78,6 @@ if len(myFile) > 0:
         #Set drivers (Browser = Chrome)
         #to remove irrelevant log error
         options = webdriver.ChromeOptions()
-        
         preferences = {
             "download.default_directory" : download_folder
             }
@@ -80,22 +88,23 @@ if len(myFile) > 0:
         #Ouverture de l'url via le driver
         driver.implicitly_wait(5)
         driver.get(url)
+        time.sleep(2)
 
+        driver.implicitly_wait(5)
         ### Connexion, changement de tab, alimentation
-
         # Store the ID of the original window (tab)
         original_window = driver.current_window_handle
         #input user + pwd
-        driver.find_elements_by_id("login")[0].send_keys(username)
-        driver.find_elements_by_id("login-pw")[0].send_keys(pwd)
+        driver.find_elements(By.ID, "login")[0].send_keys(username)
+        driver.find_elements(By.ID, "login-pw")[0].send_keys(pwd)
         #wait then log in
         driver.implicitly_wait(5)
-        driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div[3]/div[4]/div/form/div[4]/button").click()
+        driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[3]/div[4]/div/form/div[4]/button").click()
         #wait then open Activité page in new tab
         time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="menu_2_navItem"]').click()
+        driver.find_element(By.XPATH, '//*[@id="menu_2_navItem"]').click()
         driver.implicitly_wait(5)
-        driver.find_element_by_xpath('//*[@id="menu_2_ttd_0_0"]/span[2]').click()
+        driver.find_element(By.XPATH, '//*[@id="menu_2_ttd_0_0"]/span[2]').click()
         time.sleep(2)
         #switch to old tab
         driver.switch_to.window(original_window)
@@ -112,17 +121,17 @@ if len(myFile) > 0:
         for date in myFile:
             #open declaration event
             driver.implicitly_wait(5)
-            driver.find_element_by_xpath('//*[@id="btn_acc_add_label"]').click()
+            driver.find_element(By.XPATH, '//*[@id="btn_acc_add_label"]').click()
             time.sleep(1)
             #select event family (Télétravail)
-            select_TT = Select(driver.find_element_by_id('evt_famille'))
+            select_TT = Select(driver.find_element(By.ID, 'evt_famille'))
             select_TT.select_by_visible_text('Télétravail')
             time.sleep(2)
             #select event nature (Télétravail)
-            select_EVT = Select(driver.find_element_by_id('evt_natSelect'))
+            select_EVT = Select(driver.find_element(By.ID, 'evt_natSelect'))
             select_EVT.select_by_visible_text('Télétravail')
             #go on datepicker
-            datepicker = driver.find_elements_by_id("evt_datedeb")[0]
+            datepicker = driver.find_elements(By.ID, "evt_datedeb")[0]
             #clear pre-registred value
             datepicker.clear()
             #send date in datepicker
@@ -130,17 +139,17 @@ if len(myFile) > 0:
             datepicker.send_keys(date)
             time.sleep(1)
             #Save button
-            driver.find_element_by_id('evt_save_label').click()
+            driver.find_element(By.ID, 'evt_save_label').click()
             time.sleep(1)
             #Get box title value in variable to testing if error 
-            titleBox = driver.find_element_by_id('dialogBoxGen_1_title')
+            titleBox = driver.find_element(By.ID, 'dialogBoxGen_1_title')
             #close title box button
-            driver.find_element_by_id('btn1_label').click()
+            driver.find_element(By.ID, 'btn1_label').click()
             #test title value
             if titleBox.text == 'Erreur':
                 logger.error('Problème avec la date: '+date)
                 #close saisir evenement for the following date
-                driver.find_element_by_id('evt_close_label').click()
+                driver.find_element(By.ID, 'evt_close_label').click()
             #if no error
             else:
                 logger.info('Date ajoutée à ADP en Télétravail: '+date)
